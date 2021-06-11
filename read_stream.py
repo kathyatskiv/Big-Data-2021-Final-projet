6db4c00461746de6324931f8ef6251937bcd854e
Yatskiv Kath <iamkate4@gmail.com>
2021-06-11 19:43:47 +0300

    stream to kafka

---
+++ read_stream.py
@@ -0,0 +1,24 @@
+import json
+import requests
+from kafka import KafkaProducer, KafkaConsumer
+from kafka.admin import KafkaAdminClient, NewTopic
+
+from config import HOST_1, HOST_2, HOST_3
+
+r = requests.get('http://stream.meetup.com/2/rsvps', stream=True)
+
+producer = KafkaProducer(bootstrap_servers=[HOST_1, HOST_2, HOST_3],
+                        value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+
+admin = KafkaAdminClient(
+    bootstrap_servers=[HOST_1, HOST_2, HOST_3], 
+    client_id='admin'
+)
+
+
+for line in r.iter_lines():
+    if line:
+        decoded_line = line.decode('utf-8')
+        data = json.loads(decoded_line)
+
+        producer.send('all_events', value=data)
